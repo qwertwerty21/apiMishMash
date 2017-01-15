f587fd9b31762a09055fd4e9cebcfbe8f13853e4
Sunny <sunnyw1212@gmail.com>
2017-01-14 19:06:30 -0800

    fixed mongolab url

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const mongoURL = process.env.MONGO_URL;
+const mongoURL = "mongodb://sunnyshorturl:shorturl@ds163758.mlab.com:63758/shorturl";
 const routes = require('./routes/index');
 // const users = require('./routes/users');
 
